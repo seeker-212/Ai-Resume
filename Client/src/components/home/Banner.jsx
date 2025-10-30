291ea9537f59a334ebf49036e1288c6cbc1bc906
Henry <henryugwu565@gmail.com>
2025-10-31 00:40:13 +0100

    Update: Created a hero section for the prject home page

--- Client/src/components/home/Banner.jsx
+++ Client/src/components/home/Banner.jsx
@@ -4,7 +4,7 @@ function Banner() {
   return (
     <div
       className="w-full py-2.5 font-medium text-sm text-green-800 text-center 
-    bg-gradient-to-r from-[#ABFF7E] to-[#FDFEFF]"
+    bg-linear-to-r from-[#ABFF7E] to-[#FDFEFF]"
     >
       <p>
         <span className="px-3 py-1 rounded-lg text-white bg-green-600 mr-2">
